966ed0c8406199478034136cb39ba8b83842e15c
MierafA12 <mierafabebe12@gmail.com>
2025-12-30 19:49:29 +0300

    ET-40 Integrate frontend profile with backend  by  uaerApi

---
+++ Frontend/user-front/src/api/userApi.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://localhost/EthioEvents/Backend/public",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const registerUser = (user) =>
+  API.post("/signup", {
+    name: user.name,
+    username: user.username,
+    dob: user.dob,
+    phone_number: user.phone_number,
+    email: user.email,
+    password: user.password,
+  });
+
+export const loginUser = (email, password) =>
+  API.post("/login", { email, password });
+
+export const getProfile = (jwt) =>
+  API.get("/profile", {
+    headers: { Authorization: `Bearer ${jwt}` },
+  });
+
+export const updateProfile = (jwt, data) =>
+  API.put("/profile", data, {
+    headers: { Authorization: `Bearer ${jwt}` },
+  });
+  export const changePassword = (payload, jwt) => 
+  API.post("/user/change-password", payload, {
+    headers: { Authorization: `Bearer ${jwt}` 
+    },
+  });
+
+
+export default API;
